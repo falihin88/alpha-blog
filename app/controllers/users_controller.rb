68893353a220759ca2bab5365ea3d327e1c955e2
MOHD FALIHIN AMRAN <falihin88@gmail.com>
2016-04-05 10:30:11 +1000

    signin upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
   def update
     
     if @user.update(user_params)
+
       flash[:success] = "Your account was updated succssfully"
       redirect_to articles_path
 
